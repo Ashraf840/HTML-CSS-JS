4c507da78f0e3f09919f87b64505547825f969ce
Ashraf840 <shift2unleashed2011@gmail.com>
2022-12-01 00:07:51 +0600

    Add animated hamburger-menu for mobile-version; header visible

---
+++ LESSON04/v2 (header-visible mobile menu)/build/js/main.js
@@ -0,0 +1,20 @@
+const initApp = () => {
+    // Get the DOM-elems by Ids
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    // Toggles the classnames of "hidden" & "flex" of the section with id of "mobileMenu".
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamburgerBtn.classList.toggle('toggle-btn');
+    }
+
+    // Create a click-event-listener to the hamburgerBtn, call the "toggleMenu()" func
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    // Create another click-event-listener; will respond to click if clicked anywhere in the  mobile-menu-section; not only in the CROSS-BUTTON
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+// Add an event-listener to the full document; which will indicate the full HTMLDOM has loaded;
+document.addEventListener('DOMContentLoaded', initApp);
---
+++ LESSON04/v2 (header-visible mobile menu)/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],   // NPM will look for all the files with ".html" extension; inside that look for all the classess inside those HTMLs to generate the CSS inside the "build/css/*.css" folder. What if there is a class mentioned in the JS & tailwind cannot build that inside the css-dir then JS calls the functions. Thus include all the JS file, if there are any tailwindcss-classname, it'll automatically includes inside the built-CSS-dir.
+  theme: {
+    extend: {
+      // Custom media queries
+      screens: {
+        'widescreen': { 'raw':'(min-aspect-ratio: 3/2)' },  // wider than it's tall (width:3; height:2)
+        'tallscreen': { 'raw':'(min-aspect-ratio: 13/20)' },  // twice as tall than it's width (width:1; height:2); aspect-ratio of 65% or, 13/20 of width than height
+      },
+      // Custom Animation
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
